519fe26ec35e9dbbb115a26bf0393ac48afb2d37
Alok Jha <alok4588@gmail.com>
2016-05-25 15:51:23 +0530

    Updating to version 1.3.1

--- include/GSManager.h
+++ include/GSManager.h
@@ -51,5 +51,18 @@
 /// order :<ul><li>NSDictionary jsonResponse : URL response in json format</li><li>NSError error : Error if any</li></ul>
 - (void)authorizeTransaction:(GSTransaction * __nonnull)transaction onCompletion:(void (^ __nonnull)(NSDictionary * __nullable jsonResponse, NSError * __nullable error))completion;
 
+/// Authorizes and returns appropriate response for a transaction
+///
+/// On successful transaction jsonResponse will contain transaction_token key which is to be sent to Merchant server with
+/// cart details within 1 Hr of doing the transaction.
+///
+/// \param transaction A GSTransaction object encapsulating GSUser and transaction amount
+///
+/// \param orderID The order id of the transaction
+///
+/// \param completion Completion block for the response with following arguments in
+/// order :<ul><li>NSDictionary jsonResponse : URL response in json format</li><li>NSError error : Error if any</li></ul>
+- (void)authorizeTransaction:(GSTransaction * __nonnull)transaction withOrderId:(NSString * __nullable)orderID onCompletion:(void (^ __nonnull)(NSDictionary * __nullable jsonResponse, NSError * __nullable error))completion;
+
 
 @end
